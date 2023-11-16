ead1cd7bb421c98dcb42fcfa19bb73816ada8b75
Markymy <rocabertemarky09@gmail.com>
2023-11-16 22:17:54 +0800

    Activity 6 ELDROID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/patigayon/activity6/MainActivity.kt
+++ app/src/main/java/com/patigayon/activity6/MainActivity.kt
@@ -4,67 +4,60 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.animation.AnimationUtils
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.patigayon.activity6.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var alertDialogSelection: TextView
-    private lateinit var datePickerSelection: TextView
-    private lateinit var timePickerSelection: TextView
-    private lateinit var customDialogSelection: TextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val buttonClick = AnimationUtils.loadAnimation(this, R.anim.button_click)
 
-        alertDialogSelection = findViewById(R.id.alertDialogSelection)
-        datePickerSelection = findViewById(R.id.datePickerSelection)
-        timePickerSelection = findViewById(R.id.timePickerSelection)
-        customDialogSelection = findViewById(R.id.customDialogSelection)
+        with(binding) {
+            alertDialogButton.setOnClickListener {
+                it.startAnimation(buttonClick)
+                showAlertDialog()
+            }
 
-        val alertDialogButton = findViewById<Button>(R.id.alertDialogButton)
-        val datePickerDialogButton = findViewById<Button>(R.id.datePickerDialogButton)
-        val timePickerDialogButton = findViewById<Button>(R.id.timePickerDialogButton)
-        val customDialogButton = findViewById<Button>(R.id.customDialogButton)
+            datePickerDialogButton.setOnClickListener {
+                it.startAnimation(buttonClick)
+                showDatePickerDialog()
+            }
 
-        alertDialogButton.setOnClickListener {
-            it.startAnimation(buttonClick)
-            showAlertDialog()
-        }
+            timePickerDialogButton.setOnClickListener {
+                it.startAnimation(buttonClick)
+                showTimePickerDialog()
+            }
 
-        datePickerDialogButton.setOnClickListener {
-            it.startAnimation(buttonClick)
-            showDatePickerDialog()
-        }
-
-        timePickerDialogButton.setOnClickListener {
-            it.startAnimation(buttonClick)
-            showTimePickerDialog()
-        }
-
-        customDialogButton.setOnClickListener {
-            it.startAnimation(buttonClick)
-            showCustomDialog()
+            customDialogButton.setOnClickListener {
+                it.startAnimation(buttonClick)
+                showCustomDialog()
+            }
         }
     }
 
     private fun showAlertDialog() {
         val editText = EditText(this)
         val builder = AlertDialog.Builder(this)
-        builder.setTitle(getString(R.string.alert_dialog_title))
-        builder.setMessage(getString(R.string.alert_dialog_message))
+        builder.setTitle(R.string.alert_dialog_title)
+        builder.setMessage(R.string.alert_dialog_message)
         builder.setView(editText)
-        builder.setPositiveButton(getString(R.string.alert_dialog_ok)) { _, _ ->
+        builder.setPositiveButton(R.string.alert_dialog_ok) { _, _ ->
             val userInput = editText.text.toString()
-            alertDialogSelection.text = getString(R.string.alert_dialog_user_input, userInput)
-            Toast.makeText(applicationContext, getString(R.string.pressed_ok), Toast.LENGTH_SHORT).show()
+            binding.alertDialogSelection.text = getString(R.string.alert_dialog_user_input, userInput)
+            Toast.makeText(applicationContext, R.string.pressed_ok, Toast.LENGTH_SHORT).show()
         }
-        builder.setNegativeButton(getString(R.string.alert_dialog_cancel)) { _, _ ->
-            Toast.makeText(applicationContext, getString(R.string.cancelled), Toast.LENGTH_SHORT).show()
+        builder.setNegativeButton(R.string.alert_dialog_cancel) { _, _ ->
+            Toast.makeText(applicationContext, R.string.cancelled, Toast.LENGTH_SHORT).show()
         }
         builder.show()
     }
@@ -75,13 +68,11 @@ class MainActivity : AppCompatActivity() {
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-        val dpd = DatePickerDialog(this, { _, selectedYear, monthOfYear, dayOfMonth ->
-            val dateMsg = resources.getString(R.string.date_picker_title, "$dayOfMonth/${monthOfYear + 1}/$selectedYear")
-            datePickerSelection.text = dateMsg
+        DatePickerDialog(this, { _, selectedYear, monthOfYear, dayOfMonth ->
+            val dateMsg = getString(R.string.date_picker_title, "$dayOfMonth/${monthOfYear + 1}/$selectedYear")
+            binding.datePickerSelection.text = dateMsg
             Toast.makeText(applicationContext, dateMsg, Toast.LENGTH_SHORT).show()
-        }, year, month, day)
-
-        dpd.show()
+        }, year, month, day).show()
     }
 
     private fun showTimePickerDialog() {
@@ -89,13 +80,11 @@ class MainActivity : AppCompatActivity() {
         val hour = calendar.get(Calendar.HOUR_OF_DAY)
         val minute = calendar.get(Calendar.MINUTE)
 
-        val tpd = TimePickerDialog(this, { _, selectedHour, selectedMinute ->
-            val timeMsg = resources.getString(R.string.time_picker_title, "$selectedHour:$selectedMinute")
-            timePickerSelection.text = timeMsg
+        TimePickerDialog(this, { _, selectedHour, selectedMinute ->
+            val timeMsg = getString(R.string.time_picker_title, "$selectedHour:$selectedMinute")
+            binding.timePickerSelection.text = timeMsg
             Toast.makeText(applicationContext, timeMsg, Toast.LENGTH_SHORT).show()
-        }, hour, minute, true)
-
-        tpd.show()
+        }, hour, minute, true).show()
     }
 
     private fun showCustomDialog() {
@@ -103,13 +92,13 @@ class MainActivity : AppCompatActivity() {
         val customDialogEditText = dialogView.findViewById<EditText>(R.id.customDialogEditText)
 
         val dialog = AlertDialog.Builder(this)
-            .setTitle(getString(R.string.custom_dialog_title))
+            .setTitle(R.string.custom_dialog_title)
             .setView(dialogView)
             .create()
 
         dialogView.findViewById<Button>(R.id.customDialogButton).setOnClickListener {
             val userInput = customDialogEditText.text.toString()
-            customDialogSelection.text = getString(R.string.custom_dialog_input_text, userInput)
+            binding.customDialogSelection.text = getString(R.string.custom_dialog_input_text, userInput)
             dialog.dismiss()
             Toast.makeText(applicationContext, getString(R.string.custom_dialog_input_text, userInput), Toast.LENGTH_SHORT).show()
         }
